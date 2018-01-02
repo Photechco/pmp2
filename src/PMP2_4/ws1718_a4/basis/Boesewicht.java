6a4d0752a8abfc76948b79aa47e2a4c5e9147aae
Photechco <me@photech.co>
2018-01-02 16:03:54 +0100

    add aufageb 4

---
+++ src/PMP2_4/ws1718_a4/basis/Boesewicht.java
@@ -0,0 +1,10 @@
+package PMP2_4.ws1718_a4.basis;
+
+import PMP2_4.ws1718_a4.assets.Assets.AssetTyp;
+
+public class Boesewicht extends Asset  {
+  
+  public Boesewicht() {
+    super(AssetTyp.BOESEWICHT);
+  }
+}
